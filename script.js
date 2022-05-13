e6bdc7ce4863123620ff88994873c351b998f58c
Merli Pierrick <pierrick.merli@hotmail.fr>
2022-05-13 18:24:14 +0200

    Updates

---
+++ script.js
@@ -0,0 +1,28 @@
+var cms = {
+    init: function () {
+        var heightBandeau = $("#bandeauHaut").innerHeight();
+        $("#corps").css('margin-top', heightBandeau);
+        $("#bandeauHaut").addClass('sticky');
+        // On teste la position de la fenêtre au chargement de la page
+        if ($(window).scrollTop() >= heightBandeau) {
+            $('#document').addClass("scrolled");
+        } else if ($(window).scrollTop() < heightBandeau) {
+            $('#document').removeClass("scrolled");
+        }
+        // Gestion de la classe fixed au scroll de la page
+        $(window).scroll(function (e) {
+            if ($(window).scrollTop() >= heightBandeau) {
+                $('#document').addClass("scrolled");
+            } else if ($(window).scrollTop() < heightBandeau) {
+                $('#document').removeClass("scrolled");
+            }
+        });
+    },
+    menu: function () {
+
+    }
+};
+$(document).ready(function(){
+    $(cms.init);
+    $(cms.menu);
+});
